62fc5bfe2f51d11573da60f7166a9f8325bc2cda
Yousef <yosefanwar690@gmail.com>
2023-08-01 10:51:14 +0300

    corrected some router links

--- src/App.js
+++ src/App.js
@@ -29,34 +29,34 @@ function App() {
     <nav className='header-nav'>
       <ul>
         {
-          index === 0 ? <li><Link to = "/" className='activated' >HOME</Link></li>
+          index === 0 ? <li><Link to = "/space-firm-site" className='activated' >HOME</Link></li>
           :
-          <li><Link to = "/" onClick={()=>{setIndex(0);}}>HOME</Link></li>
+          <li><Link to = "/space-firm-site" onClick={()=>{setIndex(0);}}>HOME</Link></li>
         }
         {
-          index === 1 ? <li><Link to= "/destination" className='activated'>DESTINATION</Link></li>
+          index === 1 ? <li><Link to= "/space-firm-site/destination" className='activated'>DESTINATION</Link></li>
           :
-          <li><Link to= "/destination" onClick={()=>{setIndex(1);}}>DESTINATION</Link></li>
+          <li><Link to= "/space-firm-site/destination" onClick={()=>{setIndex(1);}}>DESTINATION</Link></li>
         }
         {
-          index === 2 ? <li><Link to= "/crew" className='activated' >CREW</Link></li>
+          index === 2 ? <li><Link to= "/space-firm-site/crew" className='activated' >CREW</Link></li>
           :
-          <li><Link to= "/crew" onClick={()=>{setIndex(2);}}>CREW</Link></li>
+          <li><Link to= "/space-firm-site/crew" onClick={()=>{setIndex(2);}}>CREW</Link></li>
         }
         {
-          index === 3 ? <li><Link to= "/technology" className='activated' >TECHNOLOGY</Link></li> 
+          index === 3 ? <li><Link to= "/space-firm-site/technology" className='activated' >TECHNOLOGY</Link></li> 
           :
-          <li><Link to= "/technology" onClick={()=>{setIndex(3);}}>TECHNOLOGY</Link></li> 
+          <li><Link to= "/space-firm-site/technology" onClick={()=>{setIndex(3);}}>TECHNOLOGY</Link></li> 
         }
         
       </ul>
     </nav>
 
     <Routes>
-      <Route path="/space-tourism-site" element = {<Home setIndex = {setIndex}/>}/>
-      <Route path='/destination' element = {<Destination/>}/>
-      <Route path='/crew' element = {<Crew/>}/>
-      <Route path='/technology' element = {<Technology/>}/>
+      <Route path="/space-firm-site" element = {<Home setIndex = {setIndex}/>}/>
+      <Route path='/space-firm-site/destination' element = {<Destination/>}/>
+      <Route path='/space-firm-site/crew' element = {<Crew/>}/>
+      <Route path='/space-firm-site/technology' element = {<Technology/>}/>
       <Route/>
     </Routes>
     </div>
